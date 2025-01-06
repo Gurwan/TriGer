8d63e7487eb5e09f5ba367396b6186fe1e4697df
Gurwan <34190539+Gurwan@users.noreply.github.com>
2025-01-06 19:38:06 +0100

    resize icon and add icon file for the splash screen

--- app/src/main/java/com/okariastudio/triger/data/firebase/Tracking.kt
+++ app/src/main/java/com/okariastudio/triger/data/firebase/Tracking.kt
@@ -5,12 +5,16 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.logEvent
 
 class Tracking(private val context: Context) {
-    private val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+    private val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this.context)
 
     fun logGerLearned(ger: String) {
-        analytics.logEvent("ger_learned") {
-            param("deskin", 1)
-            param("ger", ger)
+        try {
+            analytics.logEvent("ger_learned") {
+                param("deskin", 1)
+                param("ger", ger)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
