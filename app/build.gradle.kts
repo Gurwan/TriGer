fbb3d8b4387d796c0f0769322b0ac4ae5c04f073
Gurwan <34190539+Gurwan@users.noreply.github.com>
2025-01-11 11:18:26 +0100

    deploy first version of tri ger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 android {
     namespace = "com.okariastudio.triger"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.okariastudio.triger"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
