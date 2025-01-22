4a53c0f5cce72656a5a13e715106c871967e9a33
Gurwan <34190539+Gurwan@users.noreply.github.com>
2025-01-22 20:42:02 +0100

    issue #8 : hide button by default and add expandable event to show/hide the button

--- app/src/main/java/com/okariastudio/triger/ui/templates/GerMinCard.kt
+++ app/src/main/java/com/okariastudio/triger/ui/templates/GerMinCard.kt
@@ -2,6 +2,7 @@ package com.okariastudio.triger.ui.templates
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -29,6 +34,8 @@ import com.okariastudio.triger.ui.theme.TriGerTheme
 
 @Composable
 fun GerMinCard(ger: Ger, modifier: Modifier = Modifier, onDeskinClick: (Ger) -> Unit = {}) {
+    var isExpanded by remember { mutableStateOf(false) }
+
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface
@@ -36,6 +43,7 @@ fun GerMinCard(ger: Ger, modifier: Modifier = Modifier, onDeskinClick: (Ger) -> 
         modifier = modifier
             .height(IntrinsicSize.Min)
             .padding(8.dp)
+            .clickable { isExpanded = !isExpanded }
     ) {
         Column(
             modifier = Modifier
@@ -65,20 +73,22 @@ fun GerMinCard(ger: Ger, modifier: Modifier = Modifier, onDeskinClick: (Ger) -> 
                 )
             }
 
-            Spacer(modifier = Modifier.height(8.dp))
+            if (isExpanded) {
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Button(
-                onClick = { onDeskinClick(ger) },
-                modifier = Modifier.padding(start = 8.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary
-                ),
-            ) {
-                if (ger.isLearned) {
-                    Text(text = stringResource(id = R.string.reviser))
-                } else {
-                    Text(text = stringResource(id = R.string.apprendre))
+                Button(
+                    onClick = { onDeskinClick(ger) },
+                    modifier = Modifier.padding(start = 8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                ) {
+                    if (ger.isLearned) {
+                        Text(text = stringResource(id = R.string.reviser))
+                    } else {
+                        Text(text = stringResource(id = R.string.apprendre))
+                    }
                 }
             }
         }
