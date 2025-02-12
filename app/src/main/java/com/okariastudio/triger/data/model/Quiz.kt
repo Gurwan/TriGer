dcbb174e4f51e92651266075ff0f3b353882e758
Gurwan <34190539+Gurwan@users.noreply.github.com>
2025-02-12 19:55:12 +0100

    issue #11 : add enumerations for quiz feature

--- app/src/main/java/com/okariastudio/triger/data/model/Quiz.kt
+++ app/src/main/java/com/okariastudio/triger/data/model/Quiz.kt
@@ -5,3 +5,22 @@ data class Quiz(
     val exactWord: Ger? = null,
     val score: Int = 0
 )
+
+enum class QuizType {
+    WRITE,
+    CHOICE,
+    BOTH
+}
+
+enum class QuizLimit {
+    N_WORDS,
+    X_MINUTES,
+    NO_LIMIT
+}
+
+enum class QuizTarget {
+    ALL_WORDS,
+    RECENT_WORDS,
+    OLD_WORDS,
+    LEVEL_LEARNING
+}
